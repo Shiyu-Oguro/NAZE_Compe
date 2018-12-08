0298a206e1f6eb8c75deb61692c1a630364ea638
Shiyu Oguro <35251961+Shiyu-Oguro@users.noreply.github.com>
2018-12-08 20:40:35 +0900

    ステッピングモーター制御用
    アクリル板を回転させるためにステッピングモーターを用いました。
    その回転制御をするためのプログラムです。

---
+++ NAZE_Compe/NAZE_Compe.ino
@@ -0,0 +1,24 @@
+#include <MsTimer2.h>
+#define DIR_PIN 2
+#define STEP_PIN 3
+void setup() {
+  Serial.begin(115200);
+  // put your setup code here, to run once:
+  MsTimer2::set(10, tick);/*[ms]*/
+  MsTimer2::start();
+  pinMode(DIR_PIN, OUTPUT);
+  pinMode(STEP_PIN, OUTPUT);
+  digitalWrite(DIR_PIN, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+void tick()//タイマで割り込む
+{
+  digitalWrite(STEP_PIN, HIGH);
+  delay(0.1);//[ms]
+  digitalWrite(STEP_PIN, LOW);
+  Serial.println("Step");
+}
